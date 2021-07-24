b14be9391687c2ef5b78abf94b63819d3d6c4849
idbmdran <44304776+idbmdran@users.noreply.github.com>
2021-07-25 03:28:26 +0600

    some changes

--- cinnamon.sh
+++ cinnamon.sh
@@ -8,7 +8,7 @@ sudo reflector -c Bangladesh --save /etc/pacman.d/mirrorlist
 cd /tmp && git clone https://aur.archlinux.org/yay.git
 cd yay/ && makepkg -si --noconfirm
 
-sudo pacman -S --noconfirm xorg xorg-server xorg-server-utils lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon firefox arc-gtk-theme arc-icon-theme xed metacity zsh terminator alacritty terminus-font awesome dmenu nitrogen xorg-xrandr xorg-xinit virtualbox virtualbox-host-modules-arch virtualbox-guest-utils virtualbox-sdk awesome-terminal-fonts libreoffice bpytop pycharm-community-edition usbutils smartmontools firefox-i18n-bn libreoffice-fresh-bn gparted radeontop picom alsa-utils xterm xorg-xprop qlite kcolorchooser sublime-text ark-dark-solid deja-dup lxappearance mpv
+sudo pacman -S xorg xorg-server xorg-server-utils lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon firefox arc-gtk-theme arc-icon-theme xed metacity zsh terminator alacritty terminus-font awesome dmenu nitrogen xorg-xrandr xorg-xinit virtualbox virtualbox-host-modules-arch virtualbox-guest-utils virtualbox-sdk awesome-terminal-fonts libreoffice bpytop pycharm-community-edition usbutils smartmontools firefox-i18n-bn libreoffice-fresh-bn gparted radeontop picom alsa-utils xterm xorg-xprop qlite kcolorchooser sublime-text ark-dark-solid deja-dup lxappearance mpv
 
 sudo systemctl enable lightdm
 
